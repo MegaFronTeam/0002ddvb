0e0b994cb15ce1e2fda70f5151359d90f3ff4231
Janis <janis.paberzs18@gmail.com>
2022-01-03 21:23:44 +0200

    edit sContent

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 1. <https://megafronteam.github.io/0002ddvb/public>
 1. <https://megafronteam.github.io/0002ddvb/public/03-Service.html>
+1. <https://megafronteam.github.io/0002ddvb/public/03-Service-2.html>
 1. <https://megafronteam.github.io/0002ddvb/public/12-vac.html>
 1. <https://megafronteam.github.io/0002ddvb/public/13-contacts.html>
 1. <https://megafronteam.github.io/0002ddvb/public/17-news.html>
+1. <https://megafronteam.github.io/0002ddvb/public/18-contentPage.html>
+1. <https://megafronteam.github.io/0002ddvb/public/18-contentPage-2.html>
 
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -309,6 +309,25 @@ function eventHandler() {
 			: topNav.classList.remove('fixed');
 	}
 
+	const sArticleSlider = new Swiper('.mediaContent__slider--js', {
+
+		slidesPerView: 'auto',
+		watchOverflow: true,
+		spaceBetween: 0,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: ' .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			// renderBullet: function (index, className) {
+			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+			// }
+		},
+	});
+
 	function whenResize() {
 		setFixedNav();
 	}
