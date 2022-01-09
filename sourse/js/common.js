920d9522db6f5e95a115f48312244bb546d55480
Pavel <wol1414@gmail.com>
2022-01-10 02:22:38 +0300

    done slider

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,5 +1,5 @@
 "use strict";
-const JSCCommon = { 
+const JSCCommon = {
 	modalCall() {
 		const link = ".link-modal-js";
 
@@ -14,11 +14,11 @@ const JSCCommon = {
 			l10n: {
 				Escape: "Закрыть",
 				NEXT: "Вперед",
-				PREV: "Назад", 
-			}, 
-		}); 
-		document.querySelectorAll(".modal-close-js").forEach(el=>{
-			el.addEventListener("click", ()=>{
+				PREV: "Назад",
+			},
+		});
+		document.querySelectorAll(".modal-close-js").forEach(el => {
+			el.addEventListener("click", () => {
 				Fancybox.close();
 			})
 		})
@@ -73,7 +73,7 @@ const JSCCommon = {
 		}
 
 	},
-	mobileMenu() { 
+	mobileMenu() {
 		const menu = document.querySelector(".menu-mobile--js");
 		if (!menu) return;
 		this.toggleMenu();
@@ -114,7 +114,7 @@ const JSCCommon = {
 		if (isIE11) {
 			document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
 		}
-	}, 
+	},
 	heightwindow() {
 		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 		let vh = window.innerHeight * 0.01;
@@ -140,7 +140,7 @@ const JSCCommon = {
 				return false;
 			}
 		});
-	}, 
+	},
 	makeDDGroup() {
 		let parents = document.querySelectorAll('.dd-group-js');
 		for (let parent of parents) {
@@ -173,12 +173,12 @@ const JSCCommon = {
 };
 const $ = jQuery;
 
-function eventHandler() { 
-	JSCCommon.modalCall(); 
+function eventHandler() {
+	JSCCommon.modalCall();
 	JSCCommon.mobileMenu();
 	JSCCommon.inputMask();
-	JSCCommon.heightwindow();  
-	JSCCommon.makeDDGroup();  
+	JSCCommon.heightwindow();
+	JSCCommon.makeDDGroup();
 	var x = window.location.host;
 	let screenName;
 	screenName = document.body.dataset.bg;
@@ -232,7 +232,7 @@ function eventHandler() {
 		},
 	}
 
-	const swiper4 = new Swiper('.bread-slider-js', { 
+	const swiper4 = new Swiper('.bread-slider-js', {
 		slidesPerView: 'auto',
 		freeMode: true,
 		watchOverflow: true,
@@ -243,7 +243,7 @@ function eventHandler() {
 		// freeModeMomentum: true,
 
 	});
-	
+
 	const swiperBread = new Swiper('.sBanners__slider--js', {
 		// slidesPerView: 5,
 		...defaultSl,
@@ -281,7 +281,8 @@ function eventHandler() {
 
 	const sPorfolioSlider = new Swiper('.sPorfolioSlider__slider--js', {
 
-		slidesPerView: 'auto',
+		slidesPerView: 1,
+		loop: true,
 		watchOverflow: true,
 		spaceBetween: 0,
 		navigation: {
@@ -298,7 +299,7 @@ function eventHandler() {
 		},
 	});
 
-	$('#dropdown-toggle').click(function(){
+	$('#dropdown-toggle').click(function () {
 		$('#dropdown-menu').toggleClass('active');
 	});
 
@@ -308,19 +309,36 @@ function eventHandler() {
 			type: 'bullets',
 			clickable: true,
 		},
+		speed: 1000, 
 		autoplay: {
-			delay: 5000,
+			delay: 3000,
 		},
+		on: {
+			setTranslate(swiper, translate) {
+				const slides = swiper.slides
+				
+				console.log(swiper);
+				console.log(translate);
+				let root = document.documentElement;
+				root.style.setProperty('--slider-y', translate + "px");
+				root.style.setProperty('--slider-w', swiper.width + "px");
+				slides.forEach(function(el, index){ 
+					el.style.setProperty('--step', index); 
+				})
+			}
+		},
+		slidesPerView: 1,
+		direction: 'vertical',
 	});
 
-	document.addEventListener("click", function(event){
+	document.addEventListener("click", function (event) {
 		let btn = event.target.closest('.search-block__btn--js');
 		if (!btn) return;
 		let hiddenBlock = btn.closest(".search-block").querySelector('.search-block__hidden-block');
 		let input = btn.closest(".search-block").querySelector(".search-block__input");
 		hiddenBlock.classList.toggle("active");
 		setTimeout(() => input.focus(), 100)
-		
+
 	})
 	// $(document).on("click",'.search-block__btn--js', function(){
 	// 	$(this).parents('.search-block').find(".search-block__hidden-block").toggleClass("active")
@@ -340,3 +358,66 @@ if (document.readyState !== 'loading') {
 // 		document.body.classList.remove('loaded_hiding');
 // 	}, 500);
 // }
+
+
+// import {
+// 	S as e,
+// 	P as t,
+// 	E as a
+// } from "./vendor.3399a27c.js";
+// ! function (r, s = "horizontal") {
+// 	let i;
+// 	const o = [],
+// 		n = () => {
+// 			const {
+// 				offsetWidth: e,
+// 				offsetHeight: t
+// 			} = r, a = ((e / 2) ** 2 + (t / 2) ** 2) ** .5;
+// 			i.style.width = 4 * a + "px", i.style.height = 4 * a + "px", i.style.marginLeft = "vertical" === s ? `-${2 * a}px` : `-${a}px`, i.style.marginTop = "vertical" === s ? `-${a}px` : `-${2 * a}px`
+// 		};
+// 	i = document.createElement("div"), i.classList.add("paper-onboarding-fills"), r.prepend(i), r.querySelectorAll(".swiper-slide").forEach((e => {
+// 		const t = e.getAttribute("data-paper-bg-color") || "#000",
+// 			a = document.createElement("div");
+// 		a.classList.add("paper-onboarding-fill"), a.style.backgroundColor = t, i.appendChild(a), o.push(a)
+// 	})), n(), window.addEventListener("resize", n), new e(r.querySelector(".swiper"), {
+// 		modules: [t, a],
+// 		effect: "creative",
+// 		direction: s,
+// 		speed: 500,
+// 		resistanceRatio: 0,
+// 		grabCursor: !0,
+// 		pagination: {
+// 			el: ".swiper-pagination",
+// 			clickable: !0,
+// 			dynamicBullets: !0
+// 		},
+// 		creativeEffect: {
+// 			progressMultiplier: 2,
+// 			prev: {
+// 				opacity: 0,
+// 				translate: "vertical" === s ? [0, -128, 0] : [-128, 0, 0]
+// 			},
+// 			next: {
+// 				opacity: 0,
+// 				translate: "vertical" === s ? [0, 128, 0] : [128, 0, 0]
+// 			}
+// 		},
+// 		on: {
+// 			setTranslate: e => {
+// 				const {
+// 					slides: t
+// 				} = e;
+// 				for (let a = 0; a < t.length; a += 1) {
+// 					const e = t[a].progress,
+// 						r = 1 - Math.max(Math.min(Math.abs(e), 1), 0);
+// 					o[a].style.transform = e < 0 ? `scale(${r})` : "scale(1)"
+// 				}
+// 			},
+// 			setTransition: (e, t) => {
+// 				o.forEach((e => {
+// 					e.style.transitionDuration = `${t}ms`
+// 				}))
+// 			}
+// 		}
+// 	})
+// }(document.querySelector(".paper-onboarding"), "vertical");
