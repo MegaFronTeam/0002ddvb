4fc14be9554412b5b3b3c88ba8c7a469bb7b1840
Pavel <wol1414@gmail.com>
2022-02-04 02:38:53 +0300

    edit all

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -347,6 +347,11 @@ var swiperOptions = {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   },
+			pagination: {
+			el: ".swiper-pagination",
+			type: 'bullets',
+			clickable: true,
+		},
   on: {
     progress: function() {
       var swiper = this;
@@ -445,7 +450,7 @@ var swiper144 = new Swiper(".main-slider--js", swiperOptions);
 		document.querySelector('.filepond'),
 		{
 			// labelIdle: `Drag & Drop your picture or <span class="filepond--label-action">Browse</span>`,
-			labelIdle: `<span class="filepond--label-action">Нажмите</span> или перетащите  файлы в эту область. .rar .zip .doc .docx .pdf .jpg не более 10 мб`,
+			labelIdle: `Нажмите или перетащите  файлы в эту область. .rar .zip .doc .docx .pdf .jpg не более 10 мб`,
 		}
 	);
 
@@ -458,74 +463,3 @@ if (document.readyState !== 'loading') {
 } else {
 	document.addEventListener('DOMContentLoaded', eventHandler);
 }
-
-// window.onload = function () {
-// 	document.body.classList.add('loaded_hiding');
-// 	window.setTimeout(function () {
-// 		document.body.classList.add('loaded');
-// 		document.body.classList.remove('loaded_hiding');
-// 	}, 500);
-// }
-
-
-// import {
-// 	S as e,
-// 	P as t,
-// 	E as a
-// } from "./vendor.3399a27c.js";
-// ! function (r, s = "horizontal") {
-// 	let i;
-// 	const o = [],
-// 		n = () => {
-// 			const {
-// 				offsetWidth: e,
-// 				offsetHeight: t
-// 			} = r, a = ((e / 2) ** 2 + (t / 2) ** 2) ** .5;
-// 			i.style.width = 4 * a + "px", i.style.height = 4 * a + "px", i.style.marginLeft = "vertical" === s ? `-${2 * a}px` : `-${a}px`, i.style.marginTop = "vertical" === s ? `-${a}px` : `-${2 * a}px`
-// 		};
-// 	i = document.createElement("div"), i.classList.add("paper-onboarding-fills"), r.prepend(i), r.querySelectorAll(".swiper-slide").forEach((e => {
-// 		const t = e.getAttribute("data-paper-bg-color") || "#000",
-// 			a = document.createElement("div");
-// 		a.classList.add("paper-onboarding-fill"), a.style.backgroundColor = t, i.appendChild(a), o.push(a)
-// 	})), n(), window.addEventListener("resize", n), new e(r.querySelector(".swiper"), {
-// 		modules: [t, a],
-// 		effect: "creative",
-// 		direction: s,
-// 		speed: 500,
-// 		resistanceRatio: 0,
-// 		grabCursor: !0,
-// 		pagination: {
-// 			el: ".swiper-pagination",
-// 			clickable: !0,
-// 			dynamicBullets: !0
-// 		},
-// 		creativeEffect: {
-// 			progressMultiplier: 2,
-// 			prev: {
-// 				opacity: 0,
-// 				translate: "vertical" === s ? [0, -128, 0] : [-128, 0, 0]
-// 			},
-// 			next: {
-// 				opacity: 0,
-// 				translate: "vertical" === s ? [0, 128, 0] : [128, 0, 0]
-// 			}
-// 		},
-// 		on: {
-// 			setTranslate: e => {
-// 				const {
-// 					slides: t
-// 				} = e;
-// 				for (let a = 0; a < t.length; a += 1) {
-// 					const e = t[a].progress,
-// 						r = 1 - Math.max(Math.min(Math.abs(e), 1), 0);
-// 					o[a].style.transform = e < 0 ? `scale(${r})` : "scale(1)"
-// 				}
-// 			},
-// 			setTransition: (e, t) => {
-// 				o.forEach((e => {
-// 					e.style.transitionDuration = `${t}ms`
-// 				}))
-// 			}
-// 		}
-// 	})
-// }(document.querySelector(".paper-onboarding"), "vertical");
